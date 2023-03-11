5d129f85da2f03a3681cfb979ed1053adccda90e
DARSHIT TARAPARA <94553274+Darshittarapara@users.noreply.github.com>
2023-03-11 23:16:04 +0530

    Fix(DET, Routing) :: Set login page if user not log in

--- src/navigation/AuthRouting/AuthRouting.tsx
+++ src/navigation/AuthRouting/AuthRouting.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useEffect}from 'react'
+import { useNavigate } from "react-router-dom";
 import { Route, Routes } from 'react-router'
 import LoginWithOtp from "screen/Auth/LoginWithOtp/LoginWithOtp";
 import SignUp from "screen/Auth/SignUp/SignUp";
 import WithAuthLayout from "layout/WithAuthLayout/WithAuthLayout";
 import Login from "screen/Auth/Login/Login";
+import { useLocation } from 'react-router-dom';
+
 const AuthRouting = () => {
-alert("hello")
+const navigator = useNavigate();
+const { pathname } = useLocation();
+useEffect(() => {
+if(pathname==="/") {
+navigator("/login")
+}
+
+},[pathname])
     return (
         <Routes>
             <Route index path="/login" element={<WithAuthLayout component={Login} />} />
